5db9986b895a0461bbad062b029793df7a464f85
little-zy <91786171+little-zy@users.noreply.github.com>
2021-10-02 21:34:38 +0800

    Update aque.h

--- 基于链条的线性/aque.h
+++ 基于链条的线性/aque.h
@@ -1,38 +1,20 @@
 #include "alt.h"
+#include <iostream>
 #ifndef AQUE_H
 #define AQUE_H
 using namespace std;
 template <class Object>
 class aque : public alt<Object>
-/* 
+/*
  * based on alt
- * 
- * 
+ *
+ *
  */
 {
  public:
-    aque(Object object)
-    {
-        push(object);
-    }
-    void push(Object object)
-    {
-        this->push_back(object);
-    }
-    Object pop_out()
-    {
-        Object first;
-        if (this->size() > 0) {
-            first= (*this)[0];
-            this->del(0);
-        }
-        return first;
-    }
-    Object pop_item()
-    {
-        Object first;
-        if (this->size() > 0) { first= (*this)[0]; }
-        return first;
-    }
+    aque(Object object);
+    void push(Object object);
+    Object pop_out();
+    Object pop_item();
 };
 #endif
