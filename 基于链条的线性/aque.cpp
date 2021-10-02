211125306da92b869fff2b654dcbc2492355e20a
little-zy <91786171+little-zy@users.noreply.github.com>
2021-10-02 21:33:41 +0800

    Create aque.cpp

---
+++ 基于链条的线性/aque.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "alt.h"
+#include "aque.h"
+using namespace std;
+template <class Object>
+class aque : public alt<Object>
+/* 
+ * based on alt
+ * 
+ * 
+ */
+{
+ public:
+    aque(Object object)
+    {
+        push(object);
+    }
+    void push(Object object)
+    {
+        this->push_back(object);
+    }
+    Object pop_out()
+    {
+        Object first;
+        if (this->size() > 0) {
+            first= (*this)[0];
+            this->del(0);
+        }
+        return first;
+    }
+    Object pop_item()
+    {
+        Object first;
+        if (this->size() > 0) { first= (*this)[0]; }
+        return first;
+    }
+};
